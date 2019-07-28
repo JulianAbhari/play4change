13f001faa8981e59e3382ae89361500239337986
Julian Abhari <julian.abhari20@gmail.com>
2019-07-28 18:31:46 -0500

    Started firebase integration

--- Scripts/upload.js
+++ Scripts/upload.js
@@ -1,9 +1,38 @@
-function setup(){
-  gameInput = select("#gameInput");
-  gameInput.changed(submitGame);
+var filePaths;
+
+function setup() {
+  filePaths = [String];
+  firebase.initializeApp(firebaseConfig);
+
+  document.getElementById("gamePicker").addEventListener("change", function(event) {
+    let output = document.getElementById("listing");
+    let files = event.target.files;
+
+    for (let i = 0; i < files.length; i++) {
+      let item = document.createElement("li");
+      item.innerHTML = files[i].webkitRelativePath;
+      output.appendChild(item);
+      filePaths[i] = files[i].webkitRelativePath;
+    };
+  }, false);
 }
 
 //This is the function that runs when we submit a folder
-function submitGame(){
-  
+function submitGame() {
+  gameName = document.getElementById("gameName").value;
+  if (filePaths == null || gameName == null) {
+    createP("Please fill in all the fields");
+  } else {
+    var database = firebase.database();
+    var ref = database.ref('Games');
+    var data = {
+      gameName: gameName,
+      filePath: filePaths
+    };
+    ref.push(data);
+    // firebase.database().ref("Games").set({
+    //   gameName: gameName,
+    //   filePath: filePaths
+    // });
+  }
 }
