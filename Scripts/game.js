cfcb4a1e555ee116486f0a37801449f6df99936a
Michael Miller <Michaelmiller885@gmail.com>
2019-08-05 18:46:10 -0500

    Still trying to make the games run...

--- Scripts/game.js
+++ Scripts/game.js
@@ -1,4 +1,5 @@
-var key
+var urlKey
+var names = ["Julian", "Michael"]
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
@@ -16,11 +17,10 @@ database = firebase.database()
 
 // Setting params to the current URL which we get from the URLSearchParams object
 var params = new URLSearchParams(window.location.search)
-console.log(params.get("game"))
-key = params.get('game')
+urlKey = params.get('game')
 
 // Getting only the game with the requested key from Firebase
-var ref = database.ref('Games/' + key);
+var ref = database.ref('Games/' + urlKey);
 // Binding callbacks to a specific event called 'value'
 ref.on('value', loadFilePaths, errorData);
 
@@ -36,12 +36,12 @@ function loadFilePaths(firebaseData) {
   for (var i = 0; i < filePaths.length; i += 1) {
     // Create the complete path to the game files in our file server, and for the gameName
     // We have to parse it to replace the "%20" with " "
-    var totalFilePath = "../Games/" + key + "/" + gameName.replace(/%20/g, " ") + "/" + filePaths[i]
+    var totalFilePath = "../Games/" + urlKey + "/" + gameName.replace(/%20/g, " ") + "/" + filePaths[i]
 
     // Console.log all the files being loaded in script tags for dev purposes
     console.log("Loading the following into script tags:" + totalFilePath)
 
-    // Creating script tags for each game file and appending them to the game.html header
+    //Creating script tags for each game file and appending them to the game.html header
     var scriptElement = document.createElement("SCRIPT")
     scriptElement.setAttribute("src", totalFilePath)
     document.head.appendChild(scriptElement)
@@ -49,17 +49,17 @@ function loadFilePaths(firebaseData) {
   // IDEA: comment the p5 libraries out of game.html, then add them right
   // here AFTER adding the script
 
-  var p5SoundScript = document.createElement("SCRIPT")
-  p5SoundScript.setAttribute("src", "../Libraries/p5.sound.js")
-  document.head.appendChild(p5SoundScript)
-
-  var p5DomScript = document.createElement("SCRIPT")
-  p5DomScript.setAttribute("src", "../Libraries/p5.dom.js")
-  document.head.appendChild(p5DomScript)
-
-  var p5MainScript = document.createElement("SCRIPT")
-  p5MainScript.setAttribute("src", "../Libraries/p5.js")
-  document.head.appendChild(p5MainScript)
+  // var p5SoundScript = document.createElement("SCRIPT")
+  // p5SoundScript.setAttribute("src", "../Libraries/p5.sound.js")
+  // document.head.appendChild(p5SoundScript)
+  //
+  // var p5DomScript = document.createElement("SCRIPT")
+  // p5DomScript.setAttribute("src", "../Libraries/p5.dom.js")
+  // document.head.appendChild(p5DomScript)
+  //
+  // var p5MainScript = document.createElement("SCRIPT")
+  // p5MainScript.setAttribute("src", "../Libraries/p5.js")
+  // document.head.appendChild(p5MainScript)
 
   // OBSERVATIONS: The selected game works even by having its libraries
   // added in after the game, like what's shown above.
