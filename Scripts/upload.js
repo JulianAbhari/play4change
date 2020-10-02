278531dbf4d88506bff0cc2f8b3eb67cac0688d0
Julian Abhari <julian@Julians-MacBook-Pro.local>
2020-10-01 23:34:49 -0500

    revised upload page

--- Scripts/upload.js
+++ Scripts/upload.js
@@ -1,50 +1,94 @@
+// Initializing page variables
+var pageWidth;
+var pageHeight;
+var canvas;
+// Initializing UI Elements
+var gameUploadButton;
+var gameUploadLabel;
+var outputList;
+var thumbnailButton;
+var gameNameInputField;
+var submitGameButton;
+var submitGameLabel;
 // Declaring filePaths array to hold the path to each of the uploaded files.
 var filePaths;
 // Declaring a gameName variable which will later be set to the gameNameInputField
 var gameName;
-var studioName;
-var plays;
+var studioName = "Fiddlestix";
+var plays = 0;
 
 var fileUploadUrl;
 var filteredFiles;
 
 function setup() {
   initializeFirebase();
-  createCanvas(windowWidth - 4, windowHeight - 4);
-  fill(0);
+  pageWidth = windowWidth;
+  pageHeight = windowHeight
+  canvas = createCanvas(pageWidth, pageHeight);
+  window.addEventListener("resize", resize);
+
   mainColorPallete = new ColorPallete(color(210, 210, 210), color(200, 230, 200), color(100, 150, 100));
   mainPage = new StandardPage({
     pageWidth: windowWidth,
     pageHeight: windowHeight,
-    header: "Play4Change: Upload your Game!",
+    header: "Upload your Game!",
     textSize: 30,
     colorPallete: mainColorPallete
   });
-  // Setting the HTML gameUploader element's callback function
-  // This executes when the user has selected which files to upload.
-  document.getElementById("gameUploader").addEventListener("change", function(event) {
-    // Declaring an output variable to access the HTML fileListing elements
-    let output = document.getElementById("fileListing");
-    output.innerHTML = ""
-    // Declaring the files to the value that the even returned.
+
+  outputList = new p5.Element(document.getElementById("fileListing"));
+  outputList.position(0, (pageHeight / 5) * 3 + 100);
+  let item = document.createElement("li");
+  item.appendChild(document.createTextNode("Successfully Loaded Files: "));
+  outputList.child(item);
+
+  gameUploadButton = select('#gameUploadButton');
+  gameUploadButton.attribute('class', 'inputFile');
+  gameUploadButton.position(0, 0);
+
+  gameUploadButton.changed(function(event) {
     let files = event.target.files;
-    // Iterating through all the files...
+
     for (var i = 0; i < files.length; i++) {
-      // Creating a new list element for each item
       let item = document.createElement("li");
-      // Setting the item's HTML display to the file at index i's relative path.
-      item.innerHTML = files[i].webkitRelativePath;
-      // Adding a new list element to the parent 'fileListing' element
-      output.appendChild(item);
+      item.appendChild(document.createTextNode(files[i].webkitRelativePath));
+      outputList.child(item);
     }
-  }, false);
+  });
 
-  studioName = "Fiddlestix";
-  plays = 0;
-}
+  gameUploadLabel = createElement('label', 'Choose game files');
+  gameUploadLabel.attribute('class', 'fileLabel');
+  gameUploadLabel.attribute('for', 'gameUploadButton');
+  gameUploadLabel.position(pageWidth / 2 - 100, (pageHeight / 5));
 
-function draw() {
-  mainPage.draw();
+  thumbnailButton = select('#thumbnailUploader');
+  thumbnailButton.attribute('class', 'inputFile');
+  thumbnailButton.position(0, 0);
+  thumbnailButton.changed(function(event) {
+    let files = event.target.files;
+
+    for (var i = 0; i < files.length; i++) {
+      let item = document.createElement("li");
+      item.appendChild(document.createTextNode(files[i].name));
+      outputList.child(item);
+    }
+  });
+
+  thumbnailLabel = createElement('label', 'Choose box art');
+  thumbnailLabel.attribute('class', 'fileLabel');
+  thumbnailLabel.attribute('for', 'thumbnailUploader');
+  thumbnailLabel.position(pageWidth / 2 - 100, (pageHeight / 5) * 2);
+
+  gameNameInputField = select("#gameNameInputField");
+  gameNameInputField.position(pageWidth / 2 - 100, (pageHeight / 5) * 3);
+
+  submitGameButton = select("#submitGame");
+  submitGameButton.attribute('class', 'inputFile');
+  submitGameButton.position(0, 0);
+  submitGameLabel = createElement('label', 'Submit your game!');
+  submitGameLabel.attribute('class', 'fileLabel');
+  submitGameLabel.attribute('for', 'submitGame');
+  submitGameLabel.position(pageWidth / 2 - 100, (pageHeight / 5) * 4 + 100);
 }
 
 /**
@@ -54,11 +98,11 @@ function draw() {
 function filterFiles(files) {
   var sortedFiles = [];
   for (var i = 0; i < files.length; i++) {
-    switch(files[i].name){
+    switch (files[i].name) {
       case ".DS_Store":
         break;
-      // case "p5.js":
-      //   break;
+        // case "p5.js":
+        //   break;
       default:
         sortedFiles.push(files[i]);
     }
@@ -70,15 +114,16 @@ function filterFiles(files) {
 //It should be called when the user has provided their files
 //and the name of their game.
 function submitGame() {
-  gameName = document.getElementById("gameNameInputField").value;
-  filteredFiles = filterFiles(document.getElementById("gameUploader").files);
-  thumbnail = document.getElementById("thumbnailUploader").files[0];
-
   //If the user hasn't provided both their files and their game's name...
-  if (filteredFiles.length == 0 || gameName == null || thumbnail == null) {
+  if (document.getElementById("gameUploadButton").files.length == 0 || document.getElementById("thumbnailUploader").files[0] == null || document.getElementById("gameNameInputField").value == "") {
     //...a new element is displayed telling the user to fill in the fields.
-    createP("Please fill in all the fields");
+    var warning = createP("Please fill in all the fields");
+    warning.position(100, (pageHeight / 5) * 1);
+    console.log(`Value of filtered files: ${document.getElementById("gameUploader")} | Value of thumbnail: ${document.getElementById("thumbnailUploader").files[0]} | Value of gameName: ${document.getElementById("gameNameInputField").value}`);
   } else {
+    gameName = document.getElementById("gameNameInputField").value;
+    filteredFiles = filterFiles(document.getElementById("gameUploadButton").files);
+    thumbnail = document.getElementById("thumbnailUploader").files[0];
     // For each file, add its relative path within the upload folder to filePaths[]
     for (var i = 0; i < filteredFiles.length; i++) {
       var currentRelativePath = filteredFiles[i].webkitRelativePath
@@ -132,8 +177,16 @@ function submitGame() {
       .then(function(data) {
         console.log(data);
       });
+    gameUploadButton = null;
+    gameUploadLabel = null;
+    outputList = null;
+    thumbnailButton = null;
+    gameNameInputField = null;
+    submitGameButton = null;
+    submitGameLabel = null;
 
-    createP(`Game was uploaded successfully!`);
+    var success = createP("Game uploaded successfully!");
+    success.position(pageWidth / 2 - 150, pageHeight / 2);
   }
 }
 
@@ -152,4 +205,21 @@ function initializeFirebase() {
   };
   // Initializing the firebase database and it's current configuration
   firebase.initializeApp(firebaseConfig);
+}
+
+function resize() {
+  pageWidth = windowWidth;
+  pageHeight = windowHeight;
+  canvas = createCanvas(pageWidth, pageHeight);
+  mainPage.resize(pageWidth, pageHeight);
+  // Resizing InteractiveUI
+  gameUploadLabel.position(pageWidth / 2 - 100, (pageHeight / 5) * 1);
+  thumbnailLabel.position(pageWidth / 2 - 100, (pageHeight / 5) * 2);
+  gameNameInputField.position(pageWidth / 2 - 100, (pageHeight / 5) * 3);
+  outputList.position(0, (pageHeight / 5) * 3 + 100);
+  submitGameLabel.position(pageWidth / 2 - 100, (pageHeight / 5) * 4 + 100);
+}
+
+function draw() {
+  mainPage.draw();
 }
